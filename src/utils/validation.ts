053a19c4c0476c03ad8bd747038b8a4add0d4135
Sharon <sharon@MacBook-Pro-de-Sharon.local>
2024-06-23 21:20:41 -0500

    add min and max length at password

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -11,7 +11,7 @@ export const validatePassword = (password: string): ValidatePassword => {
     const specialCharRegex = /[!@#$%^&*]/;
     const digitRegex = /\d/;
     const upperCaseLetterRegex = /[A-Z]/;
-    const consecutiveLettersRegex = /([a-zA-Z])\1/;
+    const consecutiveLettersRegex = /([a-zA-Z])\1/i;
 
     const hasSpecialChar= specialCharRegex.test(password);
     const hasDigit = digitRegex.test(password);
